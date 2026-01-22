595267453084e902a0fb26633b73c304b4baf370
Anil Krishna <akumarkk@outlook.com>
2026-01-22 13:16:55 -0800

    tabs demo

--- AUI/Tabs/Tabsdemo/Program.cs
+++ AUI/Tabs/Tabsdemo/Program.cs
@@ -1,11 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Tabsdemo;
+using VisCore.Blazor.Common.Components.OverlayManager;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IOverlayManagerViewModel, OverlayManagerViewModel>();
 
 await builder.Build().RunAsync();
